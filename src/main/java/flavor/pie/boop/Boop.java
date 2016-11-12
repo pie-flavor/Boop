3a0f40f5e9412bb722102ed3e1b0feedf004b014
Adam Spofford <pieflavor.mc@gmail.com>
2016-11-11 16:11:41 -0800

    Version bump to 1.1.0

--- src/main/java/flavor/pie/boop/Boop.java
+++ src/main/java/flavor/pie/boop/Boop.java
@@ -21,7 +21,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
-@Plugin(id = "boop", name = "Boop", version = "1.0.1", authors = "pie_flavor", description = "Notifies you when you're mentioned in chat.")
+@Plugin(id = "boop", name = "Boop", version = "1.1.0", authors = "pie_flavor", description = "Notifies you when you're mentioned in chat.")
 public class Boop {
     @Inject
     Game game;
