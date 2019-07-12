a3a7fe9aa444ed248298dd65d64ed42f8efeb274
Adam Spofford <pieflavor.mc@gmail.com>
2019-07-12 16:10:22 -0700

    Update gradle

---
+++ build.gradle.kts
@@ -0,0 +1,44 @@
+plugins {
+    id("flavor.pie.promptsign") version "1.1.0"
+    id("com.github.johnrengelman.shadow") version "5.1.0"
+    `java-library`
+}
+
+group = "flavor.pie"
+version = "1.6.2"
+
+repositories {
+    mavenCentral()
+    maven {
+        url = uri("https://repo.spongepowered.org/maven/")
+    }
+    maven {
+        url = uri("https://repo.codemc.org/repository/maven-public")
+    }
+}
+
+configurations {
+    implementation {
+        extendsFrom(shadow.get())
+    }
+}
+
+dependencies {
+    api("org.spongepowered:spongeapi:7.1.0")
+    shadow("org.bstats:bstats-sponge-lite:1.4")
+}
+
+tasks.jar {
+    enabled = false
+}
+tasks.build {
+    dependsOn(tasks.shadowJar)
+}
+tasks.signArchives {
+    dependsOn(tasks.shadowJar)
+}
+
+tasks.shadowJar {
+    archiveClassifier.set("")
+    configurations = listOf(project.configurations.shadow.get())
+}
